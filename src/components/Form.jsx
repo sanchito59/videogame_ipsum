4b9a79a29fefdb15cb38ec9567abf27985d64ec3
Chris Sanchez <c.sanch7@gmail.com>
2020-05-02 18:19:13 -0700

    remove unused code

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -24,36 +24,6 @@ const Label = styled.label`
   padding-left: 10px;
 `;
 
-// const Input = styled.input`
-//   font-family: "Press Start 2P", cursive;
-//   padding-bottom: 5px;
-//   background-color: black;
-//   color: white;
-//   text-shadow: -1px -1px black, 1px 1px white;
-//   width: 40px;
-//   margin: 1%;
-//   text-align: center;
-//   border: 0px;
-//   border-bottom: 2.5px solid white;
-//   @keyframes blink {
-//     50% {
-//       border-bottom-color: black;
-//     }
-//   }
-//   animation: blink 0.7s step-end infinite alternate;
-//   ::placeholder {
-//     color: white;
-//   }
-//   :focus {
-//     outline: none;
-//     border-bottom: 2.5px solid rgb(36, 189, 127);
-//     ::placeholder {
-//       color: rgb(36, 189, 127);
-//       text-shadow: -1px -1px black, 1px 1px rgb(36, 189, 127);
-//     }
-//   }
-// `;
-
 const GenerateButton = styled(Button)`
   margin-left: 20px;
   &::before {
